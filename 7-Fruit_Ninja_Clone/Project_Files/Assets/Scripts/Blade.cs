a0cca71a1e7258ab770ec47e28cd02439b6759b2
Berk Yaşar <berkyasar01@gmail.com>
2023-06-03 16:24:10 +0300

    v1.2
    -Fruit Ninja Clone added

---
+++ 7-Fruit_Ninja_Clone/Project_Files/Assets/Scripts/Blade.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    private bool slicing;
+    private Collider bladeCollider;
+    private Camera mainCamera;
+    private TrailRenderer bladeTrail;
+    
+    public Vector3 direction { get; private set; }
+    public float minSliceVelocity = 0.01f;
+    public float sliceForce = 5f;
+
+    private void Awake() 
+    {
+        mainCamera = Camera.main;
+        bladeCollider = GetComponent<Collider>();
+        bladeTrail = GetComponentInChildren<TrailRenderer>();
+    }
+
+    private void OnEnable() 
+    {
+        StopSlicing();
+    }
+
+    private void OnDisable() 
+    {
+        StopSlicing();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartSlicing();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopSlicing();
+        }
+        else if (slicing)
+        {
+            ContinueSlicing();
+        }
+    }
+
+    private void StartSlicing()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        transform.position = newPosition;
+
+        slicing = true;
+        bladeCollider.enabled = true;
+        bladeTrail.enabled = true;
+        bladeTrail.Clear();
+    }
+
+    private void StopSlicing()
+    {
+        slicing = false;
+        bladeCollider.enabled = false;
+        bladeTrail.enabled = false;
+    }
+
+    private void ContinueSlicing()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        direction = newPosition - transform.position;
+
+        float velocity = direction.magnitude / Time.deltaTime;
+        bladeCollider.enabled = velocity > minSliceVelocity;
+
+        transform.position = newPosition;
+    }
+}
---
+++ 7-Fruit_Ninja_Clone/Project_Files/Assets/Scripts/GameManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private GameObject gameOverText;
+
+    private int score;
+
+    private Blade blade;
+    private Spawner spawner;
+
+    private void Awake() 
+    {
+        blade = FindObjectOfType<Blade>();
+        spawner = FindObjectOfType<Spawner>();
+    }
+
+    private void Start() 
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        Time.timeScale = 1f;
+
+        blade.enabled = true;
+        spawner.enabled = true;
+
+        score = 0;
+        scoreText.text = score.ToString();
+
+        ClearScene();
+    }
+
+    private void ClearScene()
+    {
+        Fruit[] fruits = FindObjectsOfType<Fruit>();
+
+        foreach (Fruit fruit in fruits)
+        {
+            Destroy(fruit.gameObject);
+        }
+
+        Bomb[] bombs = FindObjectsOfType<Bomb>();
+
+        foreach (Bomb bomb in bombs)
+        {
+            Destroy(bomb.gameObject);
+        }
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount;
+        scoreText.text = score.ToString();
+    }
+
+    public void Explode()
+    {
+        blade.enabled = false;
+        spawner.enabled = false;
+
+        StartCoroutine(ExplodeSequence());
+    }
+
+    private IEnumerator ExplodeSequence()
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.clear, Color.white, t);
+
+            Time.timeScale = 1f - t;
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        gameOverText.SetActive(true);
+
+        yield return new WaitForSecondsRealtime(2f);
+
+        NewGame();
+
+        gameOverText.SetActive(false);
+
+        elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float t = Mathf.Clamp01(elapsed / duration);
+            fadeImage.color = Color.Lerp(Color.white, Color.clear, t);
+
+            elapsed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+    }
+}
